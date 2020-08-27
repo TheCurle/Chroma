ed89a1fb77696b8e379e917c0d888c12dd234049
Curle <curle@gemwire.uk>
2020-08-27 02:08:03 +0100

    Remove ISO components

--- makeiso.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# generate our iso image :D
-# copy the elf to where grub wants it 
-# and delete any old ones for good measure first
-rm iso/boot/initrd
-rm sync.iso
-cp kernel.elf iso/boot/initrd
-#xorriso -as mkisofs -no-emul-boot -U -b boot/bootloader -hide boot/bootloader -V "Chroma v4a" -iso-level 3 -o chroma.iso iso/
-grub-mkrescue -o sync.iso iso
