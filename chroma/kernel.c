f30a0ebd689671cd2bb0740742c9cb3f6e26dbf6
Curle <curle@gemwire.uk>
2020-09-03 21:37:03 +0100

    Add defs required for kernel to compile

--- chroma/kernel.c
+++ chroma/kernel.c
@@ -1,4 +1,5 @@
 #include <kernel/chroma.h>
+#include <kernel/system/screen.h>
 
 /************************
  *** Team Kitty, 2020 ***
@@ -15,16 +16,17 @@
 size_t KernelAddr = (size_t) &LoadAddr;
 size_t KernelEnd = (size_t) &end;
 
+address_space_t KernelAddressSpace;
 
 int Main(void) {
-
+    KernelAddressSpace = (address_space_t) {0};
 
     SerialPrintf("\r\nBooting Chroma..\r\n");
     SerialPrintf("Kernel loaded at 0x%p, ends at 0x%p, is %d bytes long.\r\n", KernelAddr, KernelEnd, KernelEnd - KernelAddr);
 
     SerialPrintf("The bootloader has put the paging tables at 0x%p.\r\n", ReadControlRegister(3));
 
-    TraversePageTables();
+    //TraversePageTables();
 
     ListMemoryMap();
 
@@ -39,7 +41,8 @@ int Main(void) {
     PCIEnumerate();
 
     InitMemoryManager();
-    MemoryTest();
+
+    DrawSplash();
 
     InitPaging();
 
@@ -50,6 +53,10 @@ int Main(void) {
     
 }
 
+void SomethingWentWrong(const char* Message) {
+    SerialPrintf("Assertion failed! %s\r\n", Message);
+    //for(;;){}
+}
 
 void Exit(int ExitCode) {
     SerialPrintf("Kernel stopped with code %x\r\n", ExitCode);
