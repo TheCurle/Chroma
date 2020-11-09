a9c0fcdfde058e90fe361ca9da24b56fc6db1b50
Curle <curle@gemwire.uk>
2020-11-09 18:44:42 +0000

    Adjust the crt0 file to insert a null stack frame before calling the kernel. Provides a solid base for the stack unwinding.

--- global/crt0.c
+++ global/crt0.c
@@ -6,6 +6,8 @@ extern void _init();
 
 void _start() {
     _init();
+    unsigned long long zero = 0;
+    __asm__ __volatile__("movq %%rbp, %[input]" : : [input] "m" (zero) : );
     int ex = Main();
     Exit(ex);
 }
--- global/crt0.s
+++ global/crt0.s
@@ -1,10 +1,22 @@
 .section .text
 .global _start
 .global _init
-.global main
-.global _fini
+.global Main
+.global Exit
 .type _start, @function
 _start:
+	push %rbp
+	movq %rsp, %rbp
+	sub $0x10, %rsp
+
+	xor %rax, %rax
 	call _init
-	call main
-	call _fini
+
+	xor %rbp, %rbp
+	
+	xor %rax, %rax
+	call Main
+
+	movq %rax, %rdi
+	call Exit
+	
