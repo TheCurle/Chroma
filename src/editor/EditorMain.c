28a29ecdff6be0f94ae8d2ad144ebbe65c6cf93b
Curle <curle@gemwire.uk>
2021-06-16 22:56:46 +0100

    Minor adjustments to the editor

--- src/editor/EditorMain.c
+++ src/editor/EditorMain.c
@@ -20,12 +20,18 @@ void StartEditor(int callbackID) {
     layout.ScreenWidth = PrintInfo.screenWidth;
     layout.HeaderHeight = layout.ScreenHeight / 100 * 3;
 
+    layout.TextBoxHeight = (layout.ScreenHeight - layout.HeaderHeight) / 100 * 95;
+    layout.TextBoxY = ((layout.ScreenHeight + - layout.HeaderHeight) - layout.TextBoxHeight) / 2;
+
+    layout.TextBoxWidth = layout.ScreenWidth / 100 * 95;
+    layout.TextBoxX = (layout.ScreenWidth - layout.TextBoxWidth) / 2;
+
     SetForegroundColor(0x000084);
     FillScreen();
 
     SetForegroundColor(0x00BBBBBB);
-
     DrawFilledRect(0, 0, PrintInfo.screenWidth, layout.HeaderHeight);
+    DrawFilledRect(layout.TextBoxX, layout.TextBoxY, layout.TextBoxWidth, layout.TextBoxHeight);
 
     for(;;) {}
 
